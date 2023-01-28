f7f5b8d9cd138c93af05062efc37d2e670e24978
David Coccorullo <115152854+davidcocc@users.noreply.github.com>
2023-01-28 20:32:28 +0100

    Update testSong.py

--- src/testSong.py
+++ src/testSong.py
@@ -22,7 +22,7 @@ def testTrack():
     trackName = track["name"]
     songFeatures = [featureDictionary[0]['danceability'], featureDictionary[0]['energy'], featureDictionary[0]['valence'], featureDictionary[0]['acousticness'], featureDictionary[0]['speechiness'], featureDictionary[0]['instrumentalness'], featureDictionary[0]['liveness'], featureDictionary[0]['tempo']]
     df = pd.DataFrame([songFeatures], columns=['danceability', 'energy', 'valence', 'acousticness', 'speechiness', 'instrumentalness', 'liveness', 'tempo'])
-    df.to_csv('C:\\Users\\david\\Desktop\\progettoFIA\\script_noMode\\songFeatures.csv', encoding='utf-8', index=False)
+    df.to_csv('\\songFeatures.csv', encoding='utf-8', index=False)
     normalizedSong = normalizeSong()
     plotSong(normalizedSong, trackName)
     return
